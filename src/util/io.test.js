c3e25a8c2aceac09530849b75a82628258064380
sheng yeong <chia.shengyeong@gmail.com>
2022-09-16 08:43:38 +0800

    learn mocking and testing with mocking

--- src/util/io.test.js
+++ src/util/io.test.js
@@ -1,11 +1,25 @@
-import { it, expect } from "vitest";
+import { it, expect, vi } from "vitest";
+import { promises as fs } from "fs";
 import writeData from "./io";
+
+vi.mock("fs");
+// io tests uses built in fs function.
+// mock fs will replace the real fs with a mock version. 
+// mock can mock any built in functions or third party functions.
+// mock prevents unnecessary writes to the production/staging database or runs expensive third party services
 
 it("should execute the writeFile method", () => {
   const testData = "test";
   const testFilename = "test.txt";
-  return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+
+  writeData(testData, testFilename)
+
+  // return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
   // using the toBeUndefined because if the test is rejected, it wouldn't resolve.
   // and writing in files returns undefined.
   // this test is a problem because it really writes data. there are side effects.
+
+  expect(fs.writeFile).toBeCalled();
+  // when writeData function is called and function fs mocked
+  // this test then checks if the fs.writeFile inside it is called
 });
