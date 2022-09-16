7ce634152ee6bc133a55008f34af2e0a74950329
sheng yeong <chia.shengyeong@gmail.com>
2022-09-16 09:09:30 +0800

    more mock functionalities

--- src/data.test.js
+++ src/data.test.js
@@ -4,6 +4,11 @@ import { generateReportData } from "./data";
 describe("generateReportData()", () => {
   it("should execute logFn if provided", () => {
     const logger = vi.fn();
+
+    // logger.mockImplementationOnce(() => {});
+    // use mockImplementation or mockImplementationOnce to replace the original mock function
+    // if you just want to do so for one of the it tests.
+
     generateReportData(logger);
     // this creates an empty function and tests if the logger is called.
     // because the function above runs the logger function inside the function
